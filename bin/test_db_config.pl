b4f3bb0f72518567589603b3410520ad8ecd684c
Doug Cooper <doug_cooper2000@yahoo.co.uk>
2014-07-14 16:07:06 +0100

    Continue perl DB/CGI development
    In particular, build dropdown list from values read from the database,
    included in building HTML form dynamically in add_client.pl. Once this
    is complete, add_client.html will be obsolete.

--- bin/test_db_config.pl
+++ bin/test_db_config.pl
@@ -17,16 +17,46 @@ $dbh=DBI->connect($dsn, $config{Username}, $config{Password}) ||
     die "Error opening database: $DBI::errstr\n";
 
 # Read all data from table
-$sth=$dbh->prepare("SELECT * FROM test;") || die "Prepare failed: $DBI::errstr\n";
+#$sth=$dbh->prepare("SELECT * FROM test;") || die "Prepare failed: $DBI::errstr\n";
+#$sth = $dbh->prepare("SELECT card_type, card_type_id FROM card_type;");
+#$sth->execute() || die "Couldn't execute query: $DBI::errstr\n";
+#my %cardTypes;
+#while (my ($type, $id) = $sth->fetchrow_array) {
+#    $cardTypes{$type} = $id;
+#}
 
-$sth->execute() || die "Couldn't execute query: $DBI::errstr\n";
+#my @cardTypes = @{$dbh->selectcol_arrayref("SELECT card_type FROM card_type;")};
+#my %cardTypes = %{$dbh->selectall_hashref("SELECT card_type_id, card_type FROM card_type;", "card_type")};
+my $cardTypes = $dbh->selectall_hashref("SELECT card_type_id, card_type FROM card_type;", "card_type_id");
 
-# Print it out
-while (( $id, $firstname, $lastname) = $sth->fetchrow_array) {
-    print "Person $id: $firstname $lastname\n";
+my $values_list = [sort(keys(%{$cardTypes}))];
+my $labels_list = { map {$_->{card_type_id} => $_->{card_type}} values(%{$cardTypes}) };
+
+print "@{$values_list}\n";
+
+foreach (sort keys %{$labels_list}) {
+    print "labels_list{$_}:\t$labels_list->{$_}\n";
 }
 
+#foreach (sort keys %cardTypes) {
+#    print "$_:\t$cardTypes{$_}\n";
+#}
+
+#my ($i, $j);
+#foreach $i (keys %cardTypes) {
+#    foreach $j (keys %{$cardTypes{$i}}) {
+#        print "cardTypes{$i} -> {$j}:\t$cardTypes{$i}{$j}\n";
+#    }
+#}
+
+#print "Visa Credit: $cardTypes{'Visa Debit'}\n";
+
+# Print it out
+#while (( $id, $firstname, $lastname) = $sth->fetchrow_array) {
+#    print "Person $id: $firstname $lastname\n";
+#}
+
 # Tidy up and disconnect
-$sth->finish;
+#$sth->finish;
 $dbh->disconnect || die "Failed to disconnect: $DBI::errstr\n";
 
--- perl/add_client.pl
+++ perl/add_client.pl
@@ -4,28 +4,113 @@ use strict;
 use CGI;
 use KosovoTour;
 
-my ($cgi, $dbh, $sth, %config, %insertData);
+my ($cgi, $dbh, $sth, $cardTypes, $cardTypeValues, $cardTypeLabels, %config, %insertData);
 
 # Get the config details from file
 %config = get_config();
 
-# Read user input from webpage into hash for DB insertion
+# Connect to the database
+$dbh = connect_db($config{'Database'}, $config{'Username'}, $config{'Password'});
+
+# Read the list of valid credit card types from the DB lookup table
+$cardTypes      = $dbh->selectall_hashref("SELECT card_type_id, card_type FROM card_type;", "card_type_id");
+$cardTypeValues = [sort(keys(%{$cardTypes}))];
+$cardTypeLabels = { map {$_->{card_type_id} => $_->{card_type}} values(%{$cardTypes}) };
+
+#Create and display the initial form
 $cgi = new CGI;
+print $cgi->header;
+print $cgi->start_html(-title => "Register as a new client");
+print $cgi->$h1("Please enter your details:");
+print $cgi->start_form;
+    print $cgi->p("First Name: ", textfield(-name=>'firstname'));
+    print $cgi->p("Last Name: ", textfield(-name=>'lastname'));
+    print $cgi->p("Address Line 1: ", textfield(-name=>'address_line_1'));
+    print $cgi->p("Address Line 2: ", textfield(-name=>'address_line_2'));
+    print $cgi->p("Address Line 3: ", textfield(-name=>'address_line_3'));
+    print $cgi->p("Address Line 4: ", textfield(-name=>'address_line_4'));
+    print $cgi->p("Postcode: ", textfield(-name=>'postcode'));
+    print $cgi->p("Country: ", textfield(-name=>'country'));
+    print $cgi->p("Date of birth: ", textfield(-name=>'date_of_birth'));
+    print $cgi->p("Phone Number: ", textfield(-name=>'tel_no'));
+    print $cgi->p("Email Address: ", textfield(-name=>'email_address'));
+    print $cgi->p(h2("You must register a credit or debit card with your account. Please enter details below:"));
+    print $cgi->p("Card Number: ", textfield(-name=>'card_no'));
+    print $cgi->p("Card Type: ", textfield(-name=>'card_type'));
+    print $cgi->popup_menu(-name=>'card_type', -values=>$cardTypeValues, -labels=>$cardTypeLabels);
+    print $cgi->p("Start Date: ", textfield(-name=>'start_date'));
+    print $cgi->p("Expiry Date: ", textfield(-name=>'end_date'));
+    print $cgi->p("Issue Number: ", textfield(-name=>'issue_no'));
+    print $cgi->p("CCV Code: ", textfield(-name=>'ccv_code'));
+    p(submit),
+print $cgi->end_form;
+
+print end_html();
+
+
+
+
+
+
+
+
+# Read user input from webpage into hash for DB insertion
+
 %insertData = $cgi->param();
 
-# Connect to the database
-$dbh = connect_db($config{Database}, $config{Username}, $config{Password});
+foreach ($cgi->param()) {
+    #unless ($_ == 'email_address' or $_ == 'postcode') {
+        $insertData{$_} = $cgi->param($_, ucfirst(lc($cgi->param($_))));
+    #} else {
+    #    $insertData{$_} = $cgi->param($_);
+    #}
+}
 
 # Insert the data
 # TODO - What to do about PK?
-$sth = $dbh->prepare("INSERT INTO client VALUES ($insertData{firstname}, $insertData{lastname},
-                                                 $insertData{address_line_1}, $insertData{address_line_2},
-                                                 $insertData{address_line_3}, $insertData{address_line_4},
-                                                 $insertData{postcode}, $insertData{country},
-                                                 $insertData{date_of_birth}, $insertData{tel_no},
-                                                 $insertData{email_address}");
-$sth->execute() || die "Couldn't insert data: $DBI::errstr.\n";
+#$sth = $dbh->prepare("INSERT INTO client VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+#$sth->execute('c00001', $insertData{'firstname'}, $insertData{'lastname'},
+#              $insertData{'address_line_1'}, $insertData{'address_line_2'},
+#              $insertData{'address_line_3'}, $insertData{'address_line_4'},
+#              $insertData{'postcode'}, $insertData{'country'}, $insertData{'date_of_birth'},
+#              $insertData{'tel_no'}, $insertData{'email_address'}) || die "Couldn't insert data: $DBI::errstr.\n";
+#
+print $cgi->header();
+print $cgi->start_html("Add client");
+#print $cgi->p($cgi->h1("Registration completed successfully"));
+#print $cgi->p("Congratulations $insertData{'firstname'} $insertData{'lastname'}! You are now registered with Kosovo Tours");
+foreach (keys %insertData) {
+    print $cgi->p("$_:\t$insertData{$_}\n");
+}
+print $cgi->end_html();
 
 # Tidy up and disconnect
-$sth->finish();
+#$sth->finish();
 $dbh->disconnect();
+
+sub generate_form {
+#<FORM METHOD=GET ACTION="/cgi-bin/add_client.pl">
+    return start_form,
+    h1("Please enter your details:"),
+    p("First Name: ", textfield('firstname')),
+    p("Last Name: ", textfield('lastname')),
+    p("Address Line 1: ", textfield('address_line_1')),
+    p("Address Line 2: ", textfield('address_line_2')),
+    p("Address Line 3: ", textfield('address_line_3')),
+    p("Address Line 4: ", textfield('address_line_4')),
+    p("Postcode: ", textfield('postcode')),
+    p("Country: ", textfield('country')),
+    p("Date of birth: ", textfield('date_of_birth')),
+    p("Phone Number: ", textfield('tel_no')),
+    p("Email Address: ", textfield('email_address')),
+    p(h2("You must register a credit or debit card with your account. Please enter details below:")),
+    p("Card Number: ", textfield('card_no')),
+    p("Card Type: ", textfield('card_type')),
+    p("Start Date: ", textfield('start_date')),
+    p("Expiry Date: ", textfield('end_date')),
+    p("Issue Number: ", textfield('issue_no')),
+    p("CCV Code: ", textfield('ccv_code')),
+    p(submit),
+    end_form;
+}
+
